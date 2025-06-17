4e0ac5d496e37b8d2e1425e1d28e33e3292776d9
Thomas Fraser <8thomas0fraser8@gmail.com>
2025-06-17 02:59:48 +0100

    Initial commit, added some files and added functionality to utils.py. It should now be able to both read from and write to the walks.json file, and if no such file exists offer the user to create it

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+DATAPATH = Path("data/walks.json")
+
+def load_data(): # function for reading data from walks.json
+    if not DATAPATH.exists(): # if the file doesn't exist
+        create = input("No walking data file detected. Would you like to create one? [Y/N] ").strip().lower()
+        if create == "y":
+            DATAPATH.parent.mkdir(parents=True, exist_ok=True) # create the file
+            with open(DATAPATH, "w") as file:
+                json.dump([], file)
+            print("File created successfully")
+            return []
+        else:
+            return []
+    with open(DATAPATH, "r") as file:
+        return json.load(file) # return the read data
+    
+def sava_data(data): # function for writing data to the walks.json file
+    with open(DATAPATH, "w") as file:
+        json.dump(data, file, indent=2) # writing the data, with an indent of 2 for readability
